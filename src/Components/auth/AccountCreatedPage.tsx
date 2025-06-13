8dad4ddd658b8bca7b83fd31a173158ad9281235
uzma-rasheed24 <uzma.rasheed243@gmail.com>
2025-06-13 05:13:20 -0700

    account created page styled

--- src/Components/auth/AccountCreatedPage.tsx
+++ src/Components/auth/AccountCreatedPage.tsx
@@ -1,24 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom"; // For navigation
-import { IoCheckmarkCircle } from "react-icons/io5"; // Tick mark icon
+import { Link } from "react-router-dom"; 
 import OnBoardingLayout from "./OnBoradingLayout";
+import successIcon from "@assets/media/images/reset-success-icon.png"; 
 
 const AccountCreatedPage = () => {
   return (
-    <OnBoardingLayout>
-      <div className="flex flex-col justify-center items-center min-h-screen">
-        {/* Tick mark icon */}
-        <IoCheckmarkCircle className="text-green-500 text-6xl mb-4 mx-auto" />
-        <h2 className="text-2xl font-bold text-gray-800 mb-4">
+    <OnBoardingLayout noShowLogo={true}>
+      <div className="flex flex-col justify-center items-center text-center h-full p-6">
+        {/* Success Icon */}
+        <div className="mb-4">
+          <div className="w-[86px] h-[86px] rounded-full flex items-center justify-center">
+            <img src={successIcon} alt="Success Icon" />
+          </div>
+        </div>
+
+        {/* Heading */}
+        <h2 className="text-[#1A1A1A] font-[Space Grotesk] font-bold text-[35px] leading-[140%] tracking-[0%] mb-4">
           Account Created Successfully
         </h2>
-        <p className="text-lg text-gray-600 mb-6">
-          You have successfully created your account.
+
+        {/* Success Message */}
+        <p className="font-[Geist] font-normal text-[16px] leading-[25px] tracking-[0.5%] text-[#49475A] mb-4 max-w-md">
+          You have successfully created your account
         </p>
-        {/* Link to back to login page */}
-        <Link to="/login" className="text-blue-600 underline">
-          Back to Login
-        </Link>
+
+        {/* Back to Login Button */}
+        <div className="flex justify-center">
+          <Link
+            to="/login"
+            className="text-[16px] font-[Geist] font-semibold leading-[25px] tracking-[0.5%] text-center text-[#28A2FF] underline decoration-solid decoration-[0.5px] decoration-offset-[0px]"
+          >
+            Back to Login
+          </Link>
+        </div>
       </div>
     </OnBoardingLayout>
   );
